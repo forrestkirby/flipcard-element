a0ee11cd6a62839690a62e771908245b784ce9d7
forrestkirby <18228336+forrestkirby@users.noreply.github.com>
2021-08-23 13:47:36 +0200

    improve flip handling on touch devices

--- hd-flipcard/js/hd-flipcard.js
+++ hd-flipcard/js/hd-flipcard.js
@@ -32,28 +32,46 @@ UIkit.util.ready(() => {
 
 	UIkit.util.$$('.hd-flipcard').forEach(el => {
 
-		let flipMode = el.dataset.flipmode;
+		let evtType = 'mouse',
+			flipMode = el.dataset.flipmode,
+			flipModeTouch = el.dataset.flipmodetouch;
 
-		if (flipMode.includes('hover')) {
-			if (flipMode !== 'hover, click' || !UIkit.util.hasTouch) {
-				el.addEventListener('mouseenter', e => {
-					e.currentTarget.classList.add('hd-flipcard-hover');
-				});
+		el.addEventListener('pointerenter', e => {
+			if (flipMode.includes('hover') && e.pointerType !== 'touch') {
+				e.currentTarget.classList.add('hd-flipcard-hover');
 			}
-			el.addEventListener('mouseleave', e => {
-				e.currentTarget.classList.remove('hd-flipcard-hover');
-			});
-		}
+		});
 
-		if (flipMode.includes('click')) {
-			el.addEventListener('click', e => {
+		el.addEventListener('pointerleave', e => {
+			if (flipMode.includes('hover') && e.pointerType !== 'touch') {
+				e.currentTarget.classList.remove('hd-flipcard-hover');
+			}
+		});
+
+		el.addEventListener('click', e => {
+			if (flipMode.includes('click') && evtType !== 'touch' || flipModeTouch === 'tap' && evtType === 'touch') {
 				if (e.currentTarget.classList.contains('hd-flipcard-hover')) {
 					e.currentTarget.classList.remove('hd-flipcard-hover');
 				} else {
 					e.currentTarget.classList.add('hd-flipcard-hover');
 				}
-			});
-		}
+			}
+			evtType = 'mouse';
+		});
+
+		el.addEventListener('touchstart', e => {
+			evtType = 'touch';
+			if (flipModeTouch.includes('press')) {
+				e.currentTarget.classList.add('hd-flipcard-hover');
+			}
+		});
+
+		el.addEventListener('touchend', e => {
+			if (flipModeTouch.includes('press')) {
+				e.currentTarget.classList.remove('hd-flipcard-hover');
+			}
+			evtType = 'mouse';
+		});
 
 	});
 
